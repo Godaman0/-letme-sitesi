7b90883197ad23215c84dd2d3a7f09427f31d78a
Godaman0 <ali.emir99@hotmail.com>
2021-04-16 00:00:05 +0300

    isletme sitesi

---
+++ script/script.js
@@ -0,0 +1,358 @@
+$(document).ready(function(){
+
+	var hizmetler_btn = $("#hizmet_btn1");
+
+	$("#hizmet_btn1").mouseover(function(){
+		hizmetler_btn.css("background-color","#fff");
+		hizmetler_btn.css("color","#333");
+		hizmetler_btn.css("transition","ease 0.7s");
+	});
+
+	$("#hizmet_btn1").mouseout(function(){
+		hizmetler_btn.css("background-color","#53D3DE");
+		hizmetler_btn.css("color","#fff");
+		hizmetler_btn.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu1").mouseover(function(){
+		hizmetler_btn.css("background-color","#fff");
+		hizmetler_btn.css("color","#333");
+		hizmetler_btn.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu1").mouseout(function(){
+		hizmetler_btn.css("background-color","#53D3DE");
+		hizmetler_btn.css("color","#fff");
+		hizmetler_btn.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+
+	var hizmetler_btn2 = $("#hizmet_btn2");
+
+	$("#hizmet_btn2").mouseover(function(){
+		hizmetler_btn2.css("background-color","#fff");
+		hizmetler_btn2.css("color","#333");
+		hizmetler_btn2.css("transition","ease 0.7s");
+	});
+
+	$("#hizmet_btn2").mouseout(function(){
+		hizmetler_btn2.css("background-color","#53D3DE");
+		hizmetler_btn2.css("color","#fff");
+		hizmetler_btn2.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu2").mouseover(function(){
+		hizmetler_btn2.css("background-color","#fff");
+		hizmetler_btn2.css("color","#333");
+		hizmetler_btn2.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu2").mouseout(function(){
+		hizmetler_btn2.css("background-color","#53D3DE");
+		hizmetler_btn2.css("color","#fff");
+		hizmetler_btn2.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+	var hizmetler_btn3 = $("#hizmet_btn3");
+
+	$("#hizmetler_btn3").mouseover(function(){
+		hizmetler_btn3.css("background-color","#fff");
+		hizmetler_btn3.css("color","#333");
+		hizmetler_btn3.css("transition","ease 0.7s");
+	});
+
+	$("#hizmetler_btn3").mouseout(function(){
+		hizmetler_btn3.css("background-color","#53D3DE");
+		hizmetler_btn3.css("color","#fff");
+		hizmetler_btn3.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu3").mouseover(function(){
+		hizmetler_btn3.css("background-color","#fff");
+		hizmetler_btn3.css("color","#333");
+		hizmetler_btn3.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu3").mouseout(function(){
+		hizmetler_btn3.css("background-color","#53D3DE");
+		hizmetler_btn3.css("color","#fff");
+		hizmetler_btn3.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+
+/************************/
+
+	var hizmetler_btn11 = $("#hizmet_btn11");
+
+	$("#hizmet_btn11").mouseover(function(){
+		hizmetler_btn11.css("background-color","#fff");
+		hizmetler_btn11.css("color","#333");
+		hizmetler_btn11.css("transition","ease 0.7s");
+	});
+
+	$("#hizmet_btn11").mouseout(function(){
+		hizmetler_btn11.css("background-color","#53D3DE");
+		hizmetler_btn11.css("color","#fff");
+		hizmetler_btn11.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu11").mouseover(function(){
+		hizmetler_btn11.css("background-color","#fff");
+		hizmetler_btn11.css("color","#333");
+		hizmetler_btn11.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu11").mouseout(function(){
+		hizmetler_btn11.css("background-color","#53D3DE");
+		hizmetler_btn11.css("color","#fff");
+		hizmetler_btn11.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+
+	var hizmetler_btn22 = $("#hizmet_btn22");
+
+	$("#hizmet_btn2").mouseover(function(){
+		hizmetler_btn22.css("background-color","#fff");
+		hizmetler_btn22.css("color","#333");
+		hizmetler_btn22.css("transition","ease 0.7s");
+	});
+
+	$("#hizmet_btn2").mouseout(function(){
+		hizmetler_btn22.css("background-color","#53D3DE");
+		hizmetler_btn22.css("color","#fff");
+		hizmetler_btn22.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu22").mouseover(function(){
+		hizmetler_btn22.css("background-color","#fff");
+		hizmetler_btn22.css("color","#333");
+		hizmetler_btn22.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu22").mouseout(function(){
+		hizmetler_btn22.css("background-color","#53D3DE");
+		hizmetler_btn22.css("color","#fff");
+		hizmetler_btn22.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+	var hizmetler_btn33 = $("#hizmet_btn33");
+
+	$("#hizmetler_btn33").mouseover(function(){
+		hizmetler_btn33.css("background-color","#fff");
+		hizmetler_btn33.css("color","#333");
+		hizmetler_btn33.css("transition","ease 0.7s");
+	});
+
+	$("#hizmetler_btn33").mouseout(function(){
+		hizmetler_btn33.css("background-color","#53D3DE");
+		hizmetler_btn33.css("color","#fff");
+		hizmetler_btn33.css("transition","ease 0.7s");
+	});
+
+
+	$("#hizmetler_kutu33").mouseover(function(){
+		hizmetler_btn33.css("background-color","#fff");
+		hizmetler_btn33.css("color","#333");
+		hizmetler_btn33.css("transition","ease 0.7s");
+		});
+
+	$("#hizmetler_kutu33").mouseout(function(){
+		hizmetler_btn33.css("background-color","#53D3DE");
+		hizmetler_btn33.css("color","#fff");
+		hizmetler_btn33.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+
+	var icerik_resim_11 = $("#icerik_resim_11");
+	var icerik_goz_11 = $("#icerik_goz_11");
+
+	icerik_resim_11.mouseover(function(){
+			icerik_goz_11.show();
+			icerik_resim_11.css("opacity","0.3");
+			icerik_resim_11.css("transition","ease 0.7s");
+			icerik_goz_11.css("transition","ease 0.7s");
+		});
+
+	icerik_resim_11.mouseout(function(){
+			icerik_goz_11.hide();
+			icerik_resim_11.css("opacity","1");
+			icerik_resim_11.css("transition","ease 0.7s");
+			icerik_goz_11.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+	var icerik_resim_22 = $("#icerik_resim_22");
+	var icerik_goz_22 = $("#icerik_goz_22");
+
+	icerik_resim_22.mouseover(function(){
+			icerik_goz_22.show();
+			icerik_resim_22.css("opacity","0.3");
+			icerik_resim_22.css("transition","ease 0.7s");
+			icerik_goz_22.css("transition","ease 0.7s");
+		});
+
+	icerik_resim_22.mouseout(function(){
+			icerik_goz_22.hide();
+			icerik_resim_22.css("opacity","1");
+			icerik_resim_22.css("transition","ease 0.7s");
+			icerik_goz_22.css("transition","ease 0.7s");
+		});
+
+
+
+
+	var icerik_resim_33 = $("#icerik_resim_33");
+	var icerik_goz_33 = $("#icerik_goz_33");
+
+	icerik_resim_33.mouseover(function(){
+			icerik_goz_33.show();
+			icerik_resim_33.css("opacity","0.3");
+			icerik_resim_33.css("transition","ease 0.7s");
+			icerik_goz_33.css("transition","ease 0.7s");
+		});
+
+	icerik_resim_33.mouseout(function(){
+			icerik_goz_33.hide();
+			icerik_resim_33.css("opacity","1");
+			icerik_resim_33.css("transition","ease 0.7s");
+			icerik_goz_33.css("transition","ease 0.7s");
+		});
+
+
+
+
+
+
+
+	$("#footer_face").mouseover(function(){
+		$("#footer_face").css("background-color","#028299");
+		$("#footer_face").css("transition","ease 0.7s");
+		$("#footer_face").css("cursor","pointer");
+	});
+
+	$("#footer_face").mouseout(function(){
+		$("#footer_face").css("background-color","#111");
+		$("#footer_face").css("transition","ease 0.7s");
+		$("#footer_face").css("cursor","pointer");
+	});
+
+
+	$("#footer_goo").mouseover(function(){
+		$("#footer_goo").css("background-color","#FC4839");
+		$("#footer_goo").css("transition","ease 0.7s");
+$("#footer_goo").css("cursor","pointer");
+	});
+
+	$("#footer_goo").mouseout(function(){
+		$("#footer_goo").css("background-color","#111");
+		$("#footer_goo").css("transition","ease 0.7s");
+		$("#footer_goo").css("cursor","pointer");
+	});
+
+
+
+	$("#footer_link").mouseover(function(){
+		$("#footer_link").css("background-color","#02747C");
+		$("#footer_link").css("transition","ease 0.7s");
+	$("#footer_link").css("cursor","pointer");
+	});
+
+	$("#footer_link").mouseout(function(){
+		$("#footer_link").css("background-color","#111");
+		$("#footer_link").css("transition","ease 0.7s");
+	$("#footer_link").css("cursor","pointer");
+	});
+
+
+
+	$("#footer_twit").mouseover(function(){
+		$("#footer_twit").css("background-color","#3AC9D3");
+		$("#footer_twit").css("transition","ease 0.7s");
+	$("#footer_twit").css("cursor","pointer");
+	});
+
+	$("#footer_twit").mouseout(function(){
+		$("#footer_twit").css("background-color","#111");
+		$("#footer_twit").css("transition","ease 0.7s");
+	$("#footer_twit").css("cursor","pointer");
+	});
+
+
+
+	$("#footer_vk").mouseover(function(){
+		$("#footer_vk").css("background-color","#8004E1");
+		$("#footer_vk").css("transition","ease 0.7s");
+	$("#footer_vk").css("cursor","pointer");
+	});
+
+	$("#footer_vk").mouseout(function(){
+		$("#footer_vk").css("background-color","#111");
+		$("#footer_vk").css("transition","ease 0.7s");
+		$("#footer_vk").css("cursor","pointer");
+	});
+
+
+
+
+
+
+
+
+
+
+	$("#footer_text").focus(function(){
+		$("#footer_text").css("border","solid 1px #06ADBB");
+		$("#footer_text").css("transition","ease 0.7s");
+	});
+
+	$("#footer_text").blur(function(){
+		$("#footer_text").css("border","solid 1px #111");
+		$("#footer_text").css("transition","ease 0.7s");
+	});
+
+
+	$("#footer_mail").focus(function(){
+		$("#footer_mail").css("border","solid 1px #06ADBB");
+		$("#footer_mail").css("transition","ease 0.7s");
+	});
+
+	$("#footer_mail").blur(function(){
+		$("#footer_mail").css("border","solid 1px #111");
+		$("#footer_mail").css("transition","ease 0.7s");
+	});
+
+
+
+});
